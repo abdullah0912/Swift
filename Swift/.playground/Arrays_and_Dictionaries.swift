d1bbdafe3b9a567c1d48aeb13d9abe2234c92e4f
Abdullah Kaşgar <abdullahkasgar2494@gmail.com>
2023-01-17 10:41:59 +0300

    Update Arrays_and_Dictionaries.swift

--- Swift/.playground/Arrays_and_Dictionaries.swift
+++ Swift/.playground/Arrays_and_Dictionaries.swift
@@ -1,7 +1,7 @@
 /* Create arrays and dictionaries using brackets ([]),
 and access their elements by writing the index or key in brackets.
 A comma is allowed after the last element.
-/*
+*/
 
 var _students = ["Ahmet","Abdullah","Abdulaziz","Ali","Yusuf"]
 
